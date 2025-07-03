27237eb20cc58bb3051146d360fa7370053fe988
JohnTaronites <bastirkus@gmail.com>
2025-07-03 20:43:27 +0100

    Update Obstacle.js

--- js/objects/Obstacle.js
+++ js/objects/Obstacle.js
@@ -1,15 +1,13 @@
 class Obstacle extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, x, y) {
         super(scene, x, y, 'obstacle');
-        this.scene = scene;
-
         scene.add.existing(this);
         scene.physics.add.existing(this);
 
-        this.setScale(0.1);
+        this.setScale(0.4);
         this.body.setSize(this.width * 0.8, this.height * 0.8);
 
-        // Stała prędkość w dół (np. 220 px/s)
+        // ZAWSZE na końcu konstruktora!
         this.setVelocityY(220 + Phaser.Math.Between(0, 50));
     }
 
