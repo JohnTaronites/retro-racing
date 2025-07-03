adca092d3b048ba38e44c4d266fe8e1335c82524
JohnTaronites <bastirkus@gmail.com>
2025-07-03 20:13:04 +0100

    Update Obstacle.js

--- js/objects/Obstacle.js
+++ js/objects/Obstacle.js
@@ -1,22 +1,19 @@
-class Enemy extends Phaser.Physics.Arcade.Sprite {
+class Obstacle extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, x, y) {
-        super(scene, x, y, 'enemy_car');
+        super(scene, x, y, 'obstacle');
         this.scene = scene;
-        
-        // Add enemy to scene
+
         scene.add.existing(this);
         scene.physics.add.existing(this);
-        this.setScale(0.1);
-        // Set physics properties
+
+        this.setScale(0.4); // Przykładowe skalowanie
         this.body.setSize(this.width * 0.8, this.height * 0.8);
-        
-        // Set enemy speed
-        const speed = gameSettings.roadSpeed * 60 * (1 + Math.random() * 0.5);
+
+        const speed = gameSettings.roadSpeed * 60;
         this.setVelocityY(speed);
     }
-    
+
     update() {
-        // Destroy if off screen
         if (this.y > this.scene.game.config.height + 100) {
             this.destroy();
         }
