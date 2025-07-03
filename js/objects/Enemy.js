1ce3eea6dff18a90f5cad51b49d790819ac0b6da
JohnTaronites <bastirkus@gmail.com>
2025-07-03 20:50:00 +0100

    Update Enemy.js

--- js/objects/Enemy.js
+++ js/objects/Enemy.js
@@ -3,16 +3,19 @@ class Enemy extends Phaser.Physics.Arcade.Sprite {
         super(scene, x, y, 'enemy_car');
         scene.add.existing(this);
         scene.physics.add.existing(this);
-
-        this.setScale(0.5);
-        this.body.setSize(this.width * 0.8, this.height * 0.8);
-
-        // ZAWSZE na końcu konstruktora!
-        this.setVelocityY(250 + Phaser.Math.Between(0, 80));
+        
+        this.setScale(0.2);
+        
+        // Ustaw prędkość na bardzo wysoką, stałą wartość
+        this.setVelocityY(300);
     }
-
+    
     update() {
-        if (this.y > this.scene.game.config.height + 100) {
+        // Siłowo ustaw prędkość ponownie przy każdej aktualizacji (obejście problemu)
+        this.setVelocityY(300);
+        
+        // Zniszcz gdy poza ekranem
+        if (this.y > this.scene.game.config.height + 50) {
             this.destroy();
         }
     }
