1bf4ff9e7b562677be9d866e7b448f0960f330dd
JohnTaronites <bastirkus@gmail.com>
2025-07-04 15:29:13 +0100

    Update MenuScene.js

--- js/scenes/MenuScene.js
+++ js/scenes/MenuScene.js
@@ -2,65 +2,57 @@ class MenuScene extends Phaser.Scene {
     constructor() {
         super('MenuScene');
     }
-    
+
     create() {
-        // Background
-        const bg = this.add.tileSprite(0, 0, this.game.config.width, this.game.config.height, 'menu_bg');
-        bg.setOrigin(0, 0);
+        // Pobierz wymiary ekranu gry
+        const gameWidth = this.cameras.main.width;
+        const gameHeight = this.cameras.main.height;
         
-        /* Title
-        const title = this.add.text(this.game.config.width / 2, 150, 'RETRO RACING', {
+        // Dodaj tło menu - metoda skalowania "cover"
+        const bg = this.add.image(gameWidth/2, gameHeight/2, 'menu_bg');
+        
+        // Oblicz skalę potrzebną do pokrycia całego ekranu
+        const scaleX = gameWidth / bg.width;
+        const scaleY = gameHeight / bg.height;
+        const scale = Math.max(scaleX, scaleY);
+        
+        // Ustaw skalę obrazu
+        bg.setScale(scale);
+        
+        // Opcjonalnie: Dodaj przyciemnienie, aby tekst był lepiej widoczny
+        // const overlay = this.add.rectangle(gameWidth/2, gameHeight/2, gameWidth, gameHeight, 0x000000, 0.2);
+        
+        // START GAME button - umieść na środku ekranu
+        const startButton = this.add.rectangle(gameWidth/2, gameHeight/2, 300, 80, 0xff0000);
+        const startText = this.add.text(gameWidth/2, gameHeight/2, 'START GAME', {
             fontFamily: 'Arial',
-            fontSize: '48px',
+            fontSize: '32px',
             fontStyle: 'bold',
-            color: '#ffffff',
-            stroke: '#000000',
-            strokeThickness: 6,
-            align: 'center'
-        });
-        title.setOrigin(0.5);
-        */
-        // Start button
-        const startBtn = this.add.text(this.game.config.width / 2, this.game.config.height / 2, 'START GAME', {
-            fontFamily: 'Arial',
-            fontSize: '36px',
-            fontStyle: 'bold',
-            color: '#ffffff',
-            backgroundColor: '#ff0000',
-            padding: {
-                x: 20,
-                y: 10
-            }
-        });
-        startBtn.setOrigin(0.5);
-        startBtn.setInteractive({ useHandCursor: true });
+            color: '#ffffff'
+        }).setOrigin(0.5);
         
-        // Button animation
-        this.tweens.add({
-            targets: startBtn,
-            scale: 1.1,
-            duration: 700,
-            yoyo: true,
-            repeat: -1
+        // Dodaj interakcję do przycisku
+        startButton.setInteractive();
+        startButton.on('pointerdown', () => {
+            this.scene.start('GameScene');
         });
         
-        // Instructions
-        const instructions = this.add.text(this.game.config.width / 2, this.game.config.height - 150, 
-            'Use LEFT/RIGHT arrows or touch\nthe screen to move your car.\n\nAvoid obstacles and enemy cars!', {
+        // Instrukcje gry - na dole ekranu
+        this.add.text(gameWidth/2, gameHeight - 100, 'Use LEFT/RIGHT arrows or touch\nthe screen to move your car.', {
             fontFamily: 'Arial',
-            fontSize: '20px',
+            fontSize: '18px',
             color: '#ffffff',
             stroke: '#000000',
             strokeThickness: 4,
             align: 'center'
-        });
-        instructions.setOrigin(0.5);
+        }).setOrigin(0.5);
         
-        // Start game on button click
-        startBtn.on('pointerdown', () => {
-            this.sound.play('menu_click');
-            this.sound.play('start_game');
-            this.scene.start('GameScene');
-        });
+        this.add.text(gameWidth/2, gameHeight - 50, 'Avoid obstacles and enemy cars!', {
+            fontFamily: 'Arial',
+            fontSize: '18px',
+            color: '#ffffff',
+            stroke: '#000000',
+            strokeThickness: 4
+        }).setOrigin(0.5);
     }
 }
