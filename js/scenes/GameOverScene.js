c5098b411c2e8ba38ab792d6f79df6ab878d34c4
JohnTaronites <bastirkus@gmail.com>
2025-07-04 18:10:45 +0100

    Update GameOverScene.js

--- js/scenes/GameOverScene.js
+++ js/scenes/GameOverScene.js
@@ -8,12 +8,12 @@ class GameOverScene extends Phaser.Scene {
         const gameWidth = this.cameras.main.width;
         const gameHeight = this.cameras.main.height;
         
-        // Dodaj tło menu
+        // Dodaj tło menu - metoda skalowania "cover" z dodatkowym powiększeniem o 5%
         const bg = this.add.image(gameWidth/2, gameHeight/2, 'menu_bg');
         
-        // Oblicz skalę potrzebną do pokrycia całego ekranu
-        const scaleX = (gameWidth / bg.width);
-        const scaleY = (gameHeight / bg.height);
+        // Oblicz skalę potrzebną do pokrycia całego ekranu + dodatkowe 5%
+        const scaleX = (gameWidth / bg.width) * 1.05;
+        const scaleY = (gameHeight / bg.height) * 1.05;
         const scale = Math.max(scaleX, scaleY);
         
         // Ustaw skalę obrazu
@@ -71,28 +71,17 @@ class GameOverScene extends Phaser.Scene {
         // Dodaj interakcję do przycisku
         this.input.on('pointerdown', (pointer) => {
             if (hitAreaCallback(hitArea, pointer.x, pointer.y)) {
-                // Odtwórz dźwięk
+                // NAPRAWIONO: Dodaj dźwięk start_game.wav po kliknięciu Play Again
                 this.sound.play('start_game');
                 
-                // NAPRAWIONO: Resetowanie ustawień gry
+                // Reset game settings
                 gameSettings.currentLevel = 1;
                 gameSettings.roadSpeed = gameSettings.initialRoadSpeed;
-                gameSettings.score = 0;
                 
-                // NAPRAWIONO: Pełne resetowanie gry
-                console.log("Play Again clicked - restarting game");
-                
-                // Najpierw zatrzymaj wszystkie sceny
-                this.scene.stop('GameOverScene');
-                
-                // Zniszcz i utwórz na nowo scenę gry
-                // To wymusza pełne przeładowanie wszystkich zasobów
-                if (this.scene.get('GameScene')) {
-                    this.scene.remove('GameScene');
-                }
-                
-                // Dodaj nową instancję sceny
-                this.scene.add('GameScene', GameScene, true);
+                // Małe opóźnienie przed przejściem do gry, aby dźwięk miał czas zagrać
+                this.time.delayedCall(300, () => {
+                    this.scene.start('GameScene');
+                });
             }
         });
         
