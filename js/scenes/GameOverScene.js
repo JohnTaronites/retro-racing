1da38fd878352aa531f71791f09f9002d5d714fd
JohnTaronites <bastirkus@gmail.com>
2025-07-04 15:46:12 +0100

    Update GameOverScene.js

--- js/scenes/GameOverScene.js
+++ js/scenes/GameOverScene.js
@@ -1,8 +1,8 @@
-class MenuScene extends Phaser.Scene {
+class GameOverScene extends Phaser.Scene {
     constructor() {
-        super('MenuScene');
+        super('GameOverScene');
     }
-
+    
     create() {
         // Pobierz wymiary ekranu gry
         const gameWidth = this.cameras.main.width;
@@ -19,20 +19,45 @@ class MenuScene extends Phaser.Scene {
         // Ustaw skalę obrazu
         bg.setScale(scale);
         
-        // Dodaj delikatne przyciemnienie tła pod przyciskiem
-        const buttonY = gameHeight/2 + (gameHeight * 0.15); // Przycisk obniżony o 15%
+        // Dodaj przyciemnienie na całe tło
+        const overlay = this.add.rectangle(gameWidth/2, gameHeight/2, gameWidth, gameHeight, 0x000000, 0.5);
+        
+        // GAME OVER text
+        this.add.text(gameWidth/2, gameHeight * 0.3, 'GAME OVER', {
+            fontFamily: 'Arial',
+            fontSize: '48px',
+            fontStyle: 'bold',
+            color: '#ff0000',
+            stroke: '#000000',
+            strokeThickness: 6
+        }).setOrigin(0.5);
+        
+        // Final score
+        this.add.text(gameWidth/2, gameHeight * 0.4, `SCORE: ${gameSettings.score}`, {
+            fontFamily: 'Arial',
+            fontSize: '36px',
+            fontStyle: 'bold',
+            color: '#ffffff',
+            stroke: '#000000',
+            strokeThickness: 4
+        }).setOrigin(0.5);
+        
+        // PLAY AGAIN button - z zaokrąglonymi rogami i cieniem
+        const buttonY = gameHeight * 0.6;
+        
+        // Dodaj cień pod przyciskiem
         const buttonShadow = this.add.graphics();
         buttonShadow.fillStyle(0x000000, 0.3);
         buttonShadow.fillRoundedRect(gameWidth/2 - 160, buttonY - 50, 320, 100, 20);
         buttonShadow.fillStyle(0x000000, 0.1);
         buttonShadow.fillRoundedRect(gameWidth/2 - 170, buttonY - 60, 340, 120, 30);
         
-        // START GAME button - obniżony o 15% i z zaokrąglonymi rogami
-        const startButton = this.add.graphics();
-        startButton.fillStyle(0xFFFFFF, 1);
-        startButton.fillRoundedRect(gameWidth/2 - 150, buttonY - 40, 300, 80, 15);
+        // Przycisk z zaokrąglonymi rogami
+        const restartButton = this.add.graphics();
+        restartButton.fillStyle(0xFFFFFF, 1);
+        restartButton.fillRoundedRect(gameWidth/2 - 150, buttonY - 40, 300, 80, 15);
         
-        const startText = this.add.text(gameWidth/2, buttonY, 'START GAME', {
+        const restartText = this.add.text(gameWidth/2, buttonY, 'PLAY AGAIN', {
             fontFamily: 'Arial',
             fontSize: '32px',
             fontStyle: 'bold',
@@ -46,6 +71,10 @@ class MenuScene extends Phaser.Scene {
         // Dodaj interakcję do przycisku
         this.input.on('pointerdown', (pointer) => {
             if (hitAreaCallback(hitArea, pointer.x, pointer.y)) {
+                // Reset game settings
+                gameSettings.currentLevel = 1;
+                gameSettings.roadSpeed = gameSettings.initialRoadSpeed;
+                
                 this.scene.start('GameScene');
             }
         });
@@ -53,34 +82,16 @@ class MenuScene extends Phaser.Scene {
         // Dodaj efekt najechania na przycisk
         this.input.on('pointermove', (pointer) => {
             if (hitAreaCallback(hitArea, pointer.x, pointer.y)) {
-                startButton.clear();
-                startButton.fillStyle(0xEEEEEE, 1);
-                startButton.fillRoundedRect(gameWidth/2 - 150, buttonY - 40, 300, 80, 15);
+                restartButton.clear();
+                restartButton.fillStyle(0xEEEEEE, 1);
+                restartButton.fillRoundedRect(gameWidth/2 - 150, buttonY - 40, 300, 80, 15);
                 document.body.style.cursor = 'pointer';
             } else {
-                startButton.clear();
-                startButton.fillStyle(0xFFFFFF, 1);
-                startButton.fillRoundedRect(gameWidth/2 - 150, buttonY - 40, 300, 80, 15);
+                restartButton.clear();
+                restartButton.fillStyle(0xFFFFFF, 1);
+                restartButton.fillRoundedRect(gameWidth/2 - 150, buttonY - 40, 300, 80, 15);
                 document.body.style.cursor = 'default';
             }
         });
-        
-        // Instrukcje gry - na dole ekranu
-        this.add.text(gameWidth/2, gameHeight - 100, 'Use LEFT/RIGHT arrows or touch\nthe screen to move your car.', {
-            fontFamily: 'Arial',
-            fontSize: '18px',
-            color: '#ffffff',
-            stroke: '#000000',
-            strokeThickness: 4,
-            align: 'center'
-        }).setOrigin(0.5);
-        
-        this.add.text(gameWidth/2, gameHeight - 50, 'Avoid obstacles and enemy cars!', {
-            fontFamily: 'Arial',
-            fontSize: '18px',
-            color: '#ffffff',
-            stroke: '#000000',
-            strokeThickness: 4
-        }).setOrigin(0.5);
     }
 }
